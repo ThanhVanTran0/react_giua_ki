c9f1ee7f66063fea3cbf9313e5fcfe13a208ac15
1512500 <thanhvantran0@gmail.com>
2018-11-04 13:54:03 +0700

    UPdate sign html + css

--- Source/giua_ki/src/Components/SignIn/SignIn.js
+++ Source/giua_ki/src/Components/SignIn/SignIn.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import {Button} from 'react-bootstrap';
-import { firebaseLogin} from '../../Actions';
+import { Button } from 'react-bootstrap';
+import { firebaseLogin } from '../../Actions';
 import { connect } from 'react-redux';
+import "./SignIn.css"
 
 class SignIn extends React.Component {
-   
+
     constructor(props) {
         super(props);
         this.btnLogInClick = this.btnLogInClick.bind(this);
@@ -17,17 +18,27 @@ class SignIn extends React.Component {
 
     render() {
         return (
-            <div>
-                Sign In page
-                <Button bsStyle='primary' onClick={this.btnLogInClick}>Sign in with google</Button>
+            <div className={"container"}>
+                <div className='centerdiv'>
+                <div className={"title"}>
+                    <h2>Message App</h2>
+                </div>
+                <div className={"button"}>
+                    <button className="loginBtn loginBtn--google" onClick={this.btnLogInClick}>
+                        Login with Google
+                    </button>
+                </div>
+                </div>
             </div>
-            )
+        )
     };
 }
+
 
 const mapDispatchToProps = (dispatch) => {
     return {
         login: () => dispatch(firebaseLogin()),
-}};
- 
+    }
+};
+
 export default connect(undefined, mapDispatchToProps)(SignIn);
